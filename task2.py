42166cdb22a5c981dfb49f2a337113e972b7415c
aaleksieiev <aaleksieiev@portmone.me>
2024-05-09 01:34:52 +0300

    add depth

--- task2.py
+++ task2.py
@@ -1,46 +1,29 @@
 import turtle
 
-def koch_curve(turtle, length_order):
-    """
-    Draw a Koch snowflake curve.
-    :param turtle: Turtle object
-    :param length_order: length and order of the curve
-    """
-    if length_order < 3:
-        turtle.forward(length_order)
-        return
+def koch_curve(turtle, line_length, depth):
+    if depth <= 0:
+        turtle.forward(line_length)
     else:
         for angle in [60, -120, 60, 0]:
-            koch_curve(turtle, length_order / 3)
-            turtle.left(angle)
+            koch_curve(turtle, line_length/2, depth-1)
+            turtle.right(angle)
 
 def main():
-    # Set up the window and its attributes
-    turtle.setup(width=800, height=600)
+    # Set up the window and turtle.
+    win = turtle.Screen()
+    win.bgcolor("white")
 
-    # Create turtle object
-    t = turtle.Turtle()
+    koch_turtle = turtle.Turtle()
+    koch_turtle.speed(0)  # Fastest speed.
 
-    # Increase the speed of the turtle to the maximum
-    t.speed(0)
+    # Initial positioning.
+    koch_turtle.penup()
+    koch_turtle.goto(-400, 200)
+    koch_turtle.pendown()
 
-    # Move the turtle to start position
-    t.penup()
-    t.goto(-200, 100)
-    t.pendown()
+    koch_curve(koch_turtle, 400, 7)
 
-    # set the order of the Koch curve
-    order = 40
-    size = 700
-
-    # Draw the Koch Curve
-    koch_curve(t, size)
-
-    # Hide the turtle after drawing
-    t.hideturtle()
-
-    # Keep the window open
-    turtle.mainloop()
+    turtle.done()
 
 if __name__ == "__main__":
     main()
