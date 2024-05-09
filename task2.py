21e82c8033eae24ea99fdfd62f638cf9991f8eda
aaleksieiev <aaleksieiev@portmone.me>
2024-05-09 20:22:12 +0300

    fix curve

--- task2.py
+++ task2.py
@@ -1,14 +1,24 @@
 import turtle
+import argparse
 
 def koch_curve(turtle, line_length, depth):
     if depth <= 0:
         turtle.forward(line_length)
     else:
-        for angle in [60, -120, 60, 0]:
-            koch_curve(turtle, line_length/2, depth-1)
-            turtle.right(angle)
+        koch_curve(turtle, line_length / 3, depth - 1)
+        turtle.left(60)
+        koch_curve(turtle, line_length / 3, depth - 1)
+        turtle.right(120)
+        koch_curve(turtle, line_length / 3, depth - 1)
+        turtle.left(60)
+        koch_curve(turtle, line_length / 3, depth - 1)
 
 def main():
+    parser = argparse.ArgumentParser(description="Koch snowflake")
+    parser.add_argument("-d", "--depth", required=False, default=4, help="depth")
+
+    args = parser.parse_args()
+
     # Set up the window and turtle.
     win = turtle.Screen()
     win.bgcolor("white")
@@ -18,10 +28,10 @@ def main():
 
     # Initial positioning.
     koch_turtle.penup()
-    koch_turtle.goto(-400, 200)
+    koch_turtle.goto(-300, 200)
     koch_turtle.pendown()
 
-    koch_curve(koch_turtle, 400, 7)
+    koch_curve(koch_turtle, 500, args.depth)
 
     turtle.done()
 
